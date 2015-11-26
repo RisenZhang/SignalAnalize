a61baea90dd798cf67c04e7fe8288b083fe4bbdb
risenzhang <a100223007@mail.shu.edu.tw>
2015-11-26 23:56:09 +0800

    LogSearch¥Optimization

--- LogSearch.cs
+++ LogSearch.cs
@@ -11,6 +11,7 @@ namespace StreamReader1
     public partial class LogSearch : Form
     {
         LogServiceUtility _logService = new LogServiceUtility();
+        LogSearchData _logSearchData = new LogSearchData();
 
         List<string> _oriDate = new List<string>();
         List<string> _oriTime = new List<string>();
@@ -113,57 +114,50 @@ namespace StreamReader1
 
         private void cbb_case_SelectedIndexChanged(object sender, EventArgs e)
         {
-            string[] type0 = { "COM_NORMAL_MODE", "COM_CALIBRATION_MODE" };
-            string[] type1 = { "COM_CONSOLE_STATUS", "COM_SET_PARAMETER", "COM_CLEAR_COUNTER", "COM_SAVE_CALIBRATION",
-                               "COM_SAVE_XMODEM", "COM_BURST_MODE","COM_BURST_MODE_DISABLE","COM_CLEAR_FIRED_COUNTER" };
-            string[] type2 = { "COM_POLL_CALIBRATION_1", "COM_SET_CALIBRATION_1" };
-            string[] type3 = { "COM_POLL_CALIBRATION_2", "COM_SET_CALIBRATION_2" };
-            string[] type4 = { "COM_SET_MICROPULSE" };
-
             if (cbb_case.SelectedIndex == 0)
             {
                 cbb_fuct.Items.Clear();
-                cbb_fuct.Items.AddRange(_logService.Case0);
+                cbb_fuct.Items.AddRange(_logSearchData.Case0);
 
                 cbb_type.Items.Clear();
-                cbb_type.Items.AddRange(type0);
+                cbb_type.Items.AddRange(_logSearchData.Type0);
             }
             else if (cbb_case.SelectedIndex == 1)
             {
                 cbb_fuct.Items.Clear();
-                cbb_fuct.Items.AddRange(_logService.Case1);
+                cbb_fuct.Items.AddRange(_logSearchData.Case1);
 
                 cbb_type.Items.Clear();
-                cbb_type.Items.AddRange(type1);
+                cbb_type.Items.AddRange(_logSearchData.Type1);
             }
             else if (cbb_case.SelectedIndex == 2)
             {
                 cbb_fuct.Items.Clear();
-                cbb_fuct.Items.AddRange(_logService.Case2);
+                cbb_fuct.Items.AddRange(_logSearchData.Case2);
 
                 cbb_type.Items.Clear();
-                cbb_type.Items.AddRange(type2);
+                cbb_type.Items.AddRange(_logSearchData.Type2);
             }
             else if (cbb_case.SelectedIndex == 3)
             {
                 cbb_fuct.Items.Clear();
-                cbb_fuct.Items.AddRange(_logService.Case3);
+                cbb_fuct.Items.AddRange(_logSearchData.Case3);
 
                 cbb_type.Items.Clear();
-                cbb_type.Items.AddRange(type3);
+                cbb_type.Items.AddRange(_logSearchData.Type3);
             }
             else if (cbb_case.SelectedIndex == 4)
             {
                 cbb_fuct.Items.Clear();
-                cbb_fuct.Items.AddRange(_logService.Case4);
+                cbb_fuct.Items.AddRange(_logSearchData.Case4);
 
                 cbb_type.Items.Clear();
-                cbb_type.Items.AddRange(type4);
+                cbb_type.Items.AddRange(_logSearchData.Type4);
             }
             else
             {
                 cbb_type.Items.Clear();
-                cbb_type.Items.AddRange(type4);
+                cbb_type.Items.AddRange(_logSearchData.Type4);
             }
         }
 
@@ -262,38 +256,33 @@ namespace StreamReader1
                 switch (Com_Packet_Type)
                 {
                     case 0:
-                        int[] cg0 = new int[] { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45 };
-                        int[] cb0 = new int[] { 21, 23, 25, 27 };
-                        _logService.Createlabel(panel1, _logService.Case0.Length, _logService.Case0, Com_Tx_Buffer, cg0, cb0);
+
+                        _logService.Createlabel(panel1, _logSearchData.Case0.Length, _logSearchData.Case0, Com_Tx_Buffer, _logSearchData.Cg0, _logSearchData.Cb0);
                         dec_case.Text = "0";
                         break;
 
                     case 1:
                     fill_default: ;
-                        int[] cg1 = new int[] { 2, 3, 12, 13, 20, 29, 30, 31, 32, 33, 34, 35, 40, 41, 42, 43, 44, 45 };
-                        int[] cb1 = new int[] { 4, 6, 8, 10, 14, 16, 18, 21, 23, 25, 27, 36, 38, };
-                        _logService.Createlabel(panel1, _logService.Case1.Length, _logService.Case1, Com_Tx_Buffer, cg1, cb1);
+
+                        _logService.Createlabel(panel1, _logSearchData.Case1.Length, _logSearchData.Case1, Com_Tx_Buffer, _logSearchData.Cg1, _logSearchData.Cb1);
                         dec_case.Text = "1";
                         break;
 
                     case 2:
-                        int[] cg2 = new int[] { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45 };
-                        int[] cb2 = new int[] { };
-                        _logService.Createlabel(panel1, _logService.Case2.Length, _logService.Case2, Com_Tx_Buffer, cg2, cb2);
+
+                        _logService.Createlabel(panel1, _logSearchData.Case2.Length, _logSearchData.Case2, Com_Tx_Buffer, _logSearchData.Cg2, _logSearchData.Cb2);
                         dec_case.Text = "2";
                         break;
 
                     case 3:
-                        int[] cg3 = new int[] { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45 };
-                        int[] cb3 = new int[] { };
-                        _logService.Createlabel(panel1, _logService.Case3.Length, _logService.Case3, Com_Tx_Buffer, cg3, cb3);
+
+                        _logService.Createlabel(panel1, _logSearchData.Case3.Length, _logSearchData.Case3, Com_Tx_Buffer, _logSearchData.Cg3, _logSearchData.Cb3);
                         dec_case.Text = "3";
                         break;
 
                     case 4:
-                        int[] cg4 = new int[] { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45 };
-                        int[] cb4 = new int[] { 21, 23, 25, 27 };
-                        _logService.Createlabel(panel1, _logService.Case4.Length, _logService.Case4, Com_Tx_Buffer, cg4, cb4);
+
+                        _logService.Createlabel(panel1, _logSearchData.Case4.Length, _logSearchData.Case4, Com_Tx_Buffer, _logSearchData.Cg4, _logSearchData.Cb4);
                         dec_case.Text = "4";
                         break;
 
@@ -427,9 +416,7 @@ namespace StreamReader1
                             {
                                 Draw(i);
                             }
-                            int[] cg0 = new int[] { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45 };
-                            int[] cb0 = new int[] { 21, 23, 25, 27 };
-                            //judgefuct(case0.Length, case0, Com_Tx_Buffer, cg0, cb0);
+                            Judgefuct(_logSearchData.Case0.Length, Com_Tx_Buffer, _logSearchData.Cg0, _logSearchData.Cb0, cbb_fuct.SelectedIndex, (string)cbb_judge.SelectedItem, txt_num.Text, i);
                             break;
                         case "case 1":
                             if (chk != Global.COM_NORMAL_MODE && chk != Global.COM_CALIBRATION_MODE && chk != Global.COM_POLL_CALIBRATION_1 && chk != Global.COM_SET_CALIBRATION_1 &&
@@ -437,9 +424,7 @@ namespace StreamReader1
                             {
                                 Draw(i);
                             }
-                            int[] cg1 = new int[] { 2, 3, 12, 13, 20, 29, 30, 31, 32, 33, 34, 35, 40, 41, 42, 43, 44, 45 };
-                            int[] cb1 = new int[] { 4, 6, 8, 10, 14, 16, 18, 21, 23, 25, 27, 36, 38, };
-                            Judgefuct(_logService.Case1.Length, Com_Tx_Buffer, cg1, cb1, cbb_fuct.SelectedIndex, (string)cbb_judge.SelectedItem, txt_num.Text, i);
+                            Judgefuct(_logSearchData.Case1.Length, Com_Tx_Buffer, _logSearchData.Cg1, _logSearchData.Cb1, cbb_fuct.SelectedIndex, (string)cbb_judge.SelectedItem, txt_num.Text, i);
                             break;
 
                         case "case 2":
@@ -447,27 +432,21 @@ namespace StreamReader1
                             {
                                 Draw(i);
                             }
-                            int[] cg2 = new int[] { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45 };
-                            int[] cb2 = new int[] { };
-                            //judgefuct(case2.Length, case2, Com_Tx_Buffer, cg2, cb2);
+                            Judgefuct(_logSearchData.Case2.Length, Com_Tx_Buffer, _logSearchData.Cg2, _logSearchData.Cb2, cbb_fuct.SelectedIndex, (string)cbb_judge.SelectedItem, txt_num.Text, i);
                             break;
                         case "case 3":
                             if (chk == Global.COM_POLL_CALIBRATION_2 || chk == Global.COM_SET_CALIBRATION_2)
                             {
                                 Draw(i);
                             }
-                            int[] cg3 = new int[] { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45 };
-                            int[] cb3 = new int[] { };
-                            //judgefuct(case3.Length, case3, Com_Tx_Buffer, cg3, cb3);
+                            Judgefuct(_logSearchData.Case3.Length, Com_Tx_Buffer, _logSearchData.Cg3, _logSearchData.Cb3, cbb_fuct.SelectedIndex, (string)cbb_judge.SelectedItem, txt_num.Text, i);
                             break;
                         case "case 4":
                             if (chk == Global.COM_SET_MICROPULSE)
                             {
                                 Draw(i);
                             }
-                            int[] cg4 = new int[] { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45 };
-                            int[] cb4 = new int[] { 21, 23, 25, 27 };
-                            //judgefuct(case4.Length, case4, Com_Tx_Buffer, cg4, cb4);
+                            Judgefuct(_logSearchData.Case4.Length, Com_Tx_Buffer, _logSearchData.Cg4, _logSearchData.Cb4, cbb_fuct.SelectedIndex, (string)cbb_judge.SelectedItem, txt_num.Text, i);
                             break;
                     }
 
--- LogServiceUtility.cs
+++ LogServiceUtility.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿    using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,47 +9,7 @@ namespace StreamReader1
 {
     public class LogServiceUtility
     {
-       //宣告固定使用的CASE項目
-       public string[] Case0 = new string[] { "Flags ", "Status ", "CONSOLE_VERSION ", "CONSOLE_ASCII ", "Machine_Type ", "new_cpu_code ", 
-                               "Aiming_Factor ", "Error_Flags ", "MircoPluse_factor[0] ", "MircoPluse_factor[1] ", "MircoPluse_factor[2] ", "MircoPluse_factor[3] ", 
-                               "MircoPluse_factor[4] ", "MircoPluse_factor[5] ", "0 ", "0 ", "0 ", "0 ", 
-                               "0 ", "Laser_Temperature ", "Laser_Temperature2 ", "Cavity_Temperature ", "Maximum_Power ", "MircoPluse_factor[6] ", 
-                               "MircoPluse_factor[7] ", "MircoPluse_factor[8] ", "MircoPluse_factor[9] ", "MircoPluse_factor[10] ", "MircoPluse_factor[11] ", "0 ", 
-                               "0 ", "0 ", "0 ", "0 ", "0 ", "0 ",
-                               "0 ", "0 ", "0 ", "0 " };
 
-       public string[] Case1 = new string[] { "Flags", "Status", "Fired_Number", "Power", "Duration", "Interval", 
-                               "Aiming_Level", "Flags_3", "AD_Current_Amplitude","AD_Power_Amplitude","Total_Fired_Counter","Over_Fired_Counter",
-                               "AD_Laser_Temp0","AD_Laser2_Temp0","AD_Cavity_Temp0","Maximum_Power","laser_selection","LIO_Level",
-                               "Buzzer_Level","Parameter_Number","Power_Parameter","Detector_Parameter","Error_Flags","spot_size_Value",
-                               "scan_number","MircoPluse_index","MircoPluse_SET","MircoPluse_factor","0","(AD_AXIS_X / 100)",
-                               "(AD_AXIS_Y / 100)"};
-
-       public string[] Case2 = new string[] { "Flags ","Status ","Power_array[0][0] ","Power_array[0][1] ","Power_array[0][2] ","Power_array[0][3] ",
-                               "Power_array[0][4] ","Power_array[0][5] ","Power_array[0][6] ","Power_array[0][7] ","Power_array[0][8] ","Power_array[0][9] ",
-                               "Power_array[0][10] ","Power_array[0][11] ","Power_array[1][0] ","Power_array[1][1] ","Power_array[1][2] ","Power_array[1][3] ",
-                               "Power_array[1][4] ","Power_array[1][5] ","Power_array[1][6] ","Power_array[1][7] ","Power_array[1][8] ","Power_array[1][9] ",
-                               "Power_array[1][10] ","Power_array[1][11] ","Power_array[2][0] ","Power_array[2][1] ","Power_array[2][2] ","Power_array[2][3] ",
-                               "Power_array[2][4] ","Power_array[2][5] ","Power_array[2][6] ","Power_array[2][7] ","Power_array[2][8] ","Power_array[2][9] ",
-                               "Power_array[2][10] ","Power_array[2][11] ","0;","0;","0;","0;",
-                               "0;","0;"};
-
-       public string[] Case3 = new string[] { "Flags ","Status ","Detector_array[0][0] ","Detector_array[0][1] ","Detector_array[0][2] ","Detector_array[0][3] ",
-                               "Detector_array[0][4] ","Detector_array[0][5] ","Detector_array[0][6] ","Detector_array[0][7] ","Detector_array[0][8] ","Detector_array[0][9] ",
-                               "Detector_array[0][10] ","Detector_array[0][e7lt 11] ","Detector_array[1][0] ","Detector_array[1][1] ","Detector_array[1][2] ","Detector_array[1][3] ",
-                               "Detector_array[1][4] ","Detector_array[1][5] ","Detector_array[1][6] ","Detector_array[1][7] ","Detector_array[1][8] ","Detector_array[1][9] ",
-                               "Detector_array[1][10] ","Detector_array[1][11] ","Detector_array[2][0] ","Detector_array[2][1] ","Detector_array[2][2] ","Detector_array[2][3] ",
-                               "Detector_array[2][4] ","Detector_array[2][5] ","Detector_array[2][6] ","Detector_array[2][7] ","Detector_array[2][8] ","Detector_array[2][9] ",
-                               "Detector_array[2][10] ","Detector_array[2][11] ","0","0","0","0",
-                               "0","0",};
-
-       public string[] Case4 = new string[] { "Flags ","Status ","0 ","0 ","Machine_Type ","0 ",
-                               "0 ","Error_Flags ","MircoPluse_factor[0] ","MircoPluse_factor[1] ","MircoPluse_factor[2] ","MircoPluse_factor[3] ",
-                               "MircoPluse_factor[4] ","MircoPluse_factor[5] ","0 ","0 ","0 ","0 ",
-                               "0 ","Laser_Temperature ","Laser_Temperature2 ","Cavity_Temperature ","Maximum_Power ","MircoPluse_factor[6] ",
-                               "MircoPluse_factor[7] ","MircoPluse_factor[8] ","MircoPluse_factor[9] ","MircoPluse_factor[10] ","MircoPluse_factor[11] ","0 ",
-                               "0 ","0 ","0 ","0 ","0 ","0 ",
-                               "0 ","0 ","0 ","0 ",};
 
         //動態產生控制項Label 並依照CASE型態不同進行分析
         public void Createlabel(Panel panel1 ,int nameCount, string[] nameCase, string[] packet, int[] cg, int[] cb)
